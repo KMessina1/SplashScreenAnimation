be0ae69301324ef2bd35b2e35ac4e528d0077d84
KMessina1 <KMWeb@Mac.com>
2024-01-11 16:02:05 -0500

    Initial Commit

---
+++ SplashScreenAnimation/SplashScreenAnimationApp.swift
@@ -0,0 +1,25 @@
+//
+
+/*--------------------------------------------------------------------------------------------------------------------------
+    File: SplashScreenAnimationApp.swift
+  Author: Kevin Messina
+ Created: 1/11/24
+Modified:
+ 
+©2024 Creative App Solutions, LLC. - All Rights Reserved.
+----------------------------------------------------------------------------------------------------------------------------
+NOTES:
+--------------------------------------------------------------------------------------------------------------------------*/
+
+
+
+import SwiftUI
+
+@main
+struct SplashScreenAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
