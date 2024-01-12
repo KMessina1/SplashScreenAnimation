5a10b948c7f559b02f4fa3a743daa78558bb41a5
KMessina1 <KMWeb@Mac.com>
2024-01-12 11:48:38 -0500

    Screenshots

---
+++ SplashScreenAnimation/HomeScreen.swift
@@ -0,0 +1,26 @@
+//
+
+/*--------------------------------------------------------------------------------------------------------------------------
+    File: HomeScreen.swift
+  Author: Kevin Messina
+ Created: 1/11/24
+Modified:
+ 
+©2024 Creative App Solutions, LLC. - All Rights Reserved.
+----------------------------------------------------------------------------------------------------------------------------
+NOTES:
+--------------------------------------------------------------------------------------------------------------------------*/
+
+
+
+import SwiftUI
+
+struct HomeScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeScreen()
+}
